bd0cae5c4837cb1dc3d9bbd8a5fdb12be9cb2800
AbrahamZerega <92744854+AbrahamZerega@users.noreply.github.com>
2021-10-19 11:56:55 -0500

    Este es el codigo de la practica 1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        // Definicion de variables 
+		int a = 22;
+		int b = 6;
+		int c = 3;
+		
+		// Cuando se necesita la combinacion: aacb
+		Console.Write(a);
+		Console.Write(a);
+		Console.Write(c);
+		Console.WriteLine(b);
+		
+		Console.WriteLine("----");
+		
+		// Cuando se necesita la combinacion: bca
+		Console.Write(b);
+		Console.Write(c);
+		Console.WriteLine(a);
+		
+		Console.WriteLine("----");
+
+		// Cuando se necesita la combinacion: bcbc
+		Console.Write(b);
+		Console.Write(c);
+		Console.Write(b);
+		Console.WriteLine(c);
+		
+		
+	}
+}
